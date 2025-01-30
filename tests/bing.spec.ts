ffb564068780c2b6cb83d430356c793ece24324f
Radosław <natkaniecradoslaw@gmail.com>
2025-01-30 17:42:57 +0100

    Log the first result's address

--- tests/bing.spec.ts
+++ tests/bing.spec.ts
@@ -1,14 +1,31 @@
 import { Page } from '@playwright/test';
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 const bingHomepage = "https://www.bing.com/";
-// const searchPhrase = "semrush";
-// const selectors = {}
+const searchPhrase = "semrush";
 
-const phraseSearch = async (page: Page) => {
+const selectors = {
+  homepage: {
+    searchBar: "#sb_form_q",
+  },
+  searchResults: {
+    resultsList: "#b_results li:first-of-type",
+  },
+}
+
+const phraseSearch = async (page: Page, phrase: string) => {
   await page.goto(bingHomepage);
+  await page.fill(selectors.homepage.searchBar, phrase);
+  await page.press(selectors.homepage.searchBar, "Enter");
+  await page.waitForSelector("#b_content");
 }
 
 test.beforeEach(async ({ page }) => {
-  await phraseSearch(page);
+  await phraseSearch(page, searchPhrase);
+});
+
+test("verify the first result in the default search", async ({
+  page
+}) => {
+  console.log(await page.locator(selectors.searchResults.resultsList).locator("cite").textContent());
 });

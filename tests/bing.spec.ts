4b1aa06ac11b5c26428a452c59e595bcda1b300c
Radosław <natkaniecradoslaw@gmail.com>
2025-01-30 17:53:16 +0100

    Fix flakiness of the phraseSearch and add the first result test

--- tests/bing.spec.ts
+++ tests/bing.spec.ts
@@ -3,21 +3,27 @@ import { test, expect } from '@playwright/test';
 
 const bingHomepage = "https://www.bing.com/";
 const searchPhrase = "semrush";
+const firstResultExpected = "www.semrush.com";
 
 const selectors = {
   homepage: {
     searchBar: "#sb_form_q",
+    cookieBanner: {
+      rejectionButton: "#bnp_btn_reject",
+    },
   },
   searchResults: {
+    pageContent: "#b_content",
     resultsList: "#b_results li:first-of-type",
   },
 }
 
 const phraseSearch = async (page: Page, phrase: string) => {
   await page.goto(bingHomepage);
+  await page.locator(selectors.homepage.cookieBanner.rejectionButton).click();
   await page.fill(selectors.homepage.searchBar, phrase);
   await page.press(selectors.homepage.searchBar, "Enter");
-  await page.waitForSelector("#b_content");
+  await page.waitForSelector(selectors.searchResults.pageContent);
 }
 
 test.beforeEach(async ({ page }) => {
@@ -27,5 +33,6 @@ test.beforeEach(async ({ page }) => {
 test("verify the first result in the default search", async ({
   page
 }) => {
-  console.log(await page.locator(selectors.searchResults.resultsList).locator("cite").textContent());
+  const firstResultCite = await page.locator(selectors.searchResults.resultsList).locator("cite").textContent();
+  expect(firstResultCite).toContain(firstResultExpected);
 });
